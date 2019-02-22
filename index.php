01d5ba336a2b902d65ba94626cd99e74a378b6fd
ArkadyCH <arkadychu4vaga@gmail.com>
2019-02-22 14:21:05 +0300

    + filemanager div

---
+++ index.php
@@ -0,0 +1,43 @@
+<!Doctype html>
+<html>
+<head>
+    <title>File Manager</title>
+    <meta charset="Utg-8">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="file-manager">
+    <div class="file-manager__pager">
+        <div class="file-manager__pager-slash">/</div>
+        <div class="file-manager__pager-directory--hidden">Directory Name</div>
+    </div>
+    <div class="file-manager__directories">
+        <div class="file-manager__directory">
+            <div class="file-manager__directory-folder">
+                <img class="file-manager__directory-img" src="/img/directory.png">
+            </div>
+            <div class="file-manager__directory-name">Directory 1</div>
+        </div>
+        <div class="file-manager__directory">
+            <div class="file-manager__directory-folder">
+                <img class="file-manager__directory-img" src="/img/directory.png">
+            </div>
+            <div class="file-manager__directory-name">Directory 2</div>
+        </div>
+        <div class="file-manager__directory">
+            <div class="file-manager__directory-folder">
+                <img class="file-manager__directory-img" src="/img/directory.png">
+            </div>
+            <div class="file-manager__directory-name">Directory 3</div>
+        </div>
+    </div>
+</div>
+<script
+        src="https://code.jquery.com/jquery-3.3.1.js"
+        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+        crossorigin="anonymous">
+</script>
+<script src="/js/script.js"></script>
+</body>
+</html>
